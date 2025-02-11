6abb69d7760b8948e1e624c78457877ac4f1d9e7
AmandineH <honvaultamandine@gmail.com>
2025-02-11 21:10:30 +0800

    Finish new campaign

--- pages/new-campaign.vue
+++ pages/new-campaign.vue
@@ -42,7 +42,7 @@
 
     <div class="bg-black">
       <div
-        class="mx-auto gap-24px max-w-1300px"
+        class="mx-auto max-w-1300px"
         :class="
           viewport.isLessThan('tablet')
             ? 'px-16px py-32px'
@@ -156,13 +156,29 @@
       </div>
     </div>
 
-    <iframe
-      src="https://www.juicer.io/api/feeds/nano_0ne/iframe"
-      frameborder="0"
-      width="100%"
-      height="400"
-      style="display: block; margin: 0 auto"
-    ></iframe>
+    <div
+      class="mx-auto max-w-1300px relative"
+      :class="
+        viewport.isLessThan('tablet') ? 'px-16px py-32px' : 'px-24px py-48px'
+      "
+    >
+      <div
+        class="relative"
+        :class="viewport.isLessThan('tablet') ? 'pt-16px' : ''"
+      >
+        <div class="bg-white absolute top-0 min-h-[27px] w-full right-0">
+          <p class="text-2xl font-bold mb-16px">
+            Join the Movement: See Our Creators in Action
+          </p>
+        </div>
+        <iframe
+          src="https://www.juicer.io/api/feeds/nano_0ne/iframe"
+          frameborder="0"
+          width="100%"
+          height="400"
+        ></iframe>
+      </div>
+    </div>
 
     <div
       class="mx-auto gap-24px max-w-1300px"
