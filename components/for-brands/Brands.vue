ebf95637426b2cf8af0552e80fa741c00fbc9174
AmandineH <honvaultamandine@gmail.com>
2025-02-17 19:18:14 +0800

    Add case studies

--- components/for-brands/Brands.vue
+++ components/for-brands/Brands.vue
@@ -9,6 +9,87 @@
       How We Helped Businesses Like Yours
     </p>
 
+    <UCarousel
+      :ui="{
+        container: 'gap-16px',
+        item: viewport.isLessThan('tablet') ? 'basis-full' : '',
+        arrows: {
+          wrapper: 'flex items-center justify-start gap-8px mt-8px',
+        },
+      }"
+      :items="items"
+      arrows
+      class="mb-24px"
+    >
+      <template #default="{ item }">
+        <div
+          class="bg-black rounded-lg overflow-hidden h-full"
+          :class="
+            viewport.isLessThan('tablet')
+              ? 'flex flex-col'
+              : 'min-w-[700px] w-[50vw] grid grid-cols-4'
+          "
+        >
+          <div class="bg-primary-500 flex flex-col justify-between">
+            <div
+              class="flex gap-8px justify-center"
+              :class="viewport.isLessThan('tablet') ? 'p-16px' : 'p-24px'"
+            >
+              <img
+                v-for="(item, key) in item.logos"
+                :key="key"
+                :src="item"
+                :class="
+                  viewport.isLessThan('tablet')
+                    ? 'w-1/3 max-w-[100px]'
+                    : 'w-1/2'
+                "
+              />
+            </div>
+
+            <img
+              :src="item.bg"
+              :class="
+                viewport.isLessThan('tablet')
+                  ? 'w-full h-[200px] object-cover object-top'
+                  : ''
+              "
+            />
+          </div>
+
+          <div
+            class="col-span-3"
+            :class="viewport.isLessThan('tablet') ? 'p-16px' : 'p-24px'"
+          >
+            <p class="text-white text-2xl font-bold mb-24px">
+              {{ item.title }}
+            </p>
+            <div v-html="item.description" class="text-white text-md" />
+          </div>
+        </div>
+      </template>
+
+      <template #prev="{ onClick, disabled }">
+        <button :disabled="disabled" @click="onClick">
+          <UIcon
+            name="i-heroicons-arrow-left-circle"
+            class="w-8 h-8 button"
+            :class="disabled ? 'opacity-50' : ''"
+          />
+        </button>
+      </template>
+
+      <template #next="{ onClick, disabled }">
+        <button :disabled="disabled" @click="onClick">
+          <UIcon
+            name="i-heroicons-arrow-right-circle"
+            class="w-8 h-8 button"
+            :class="disabled ? 'opacity-50' : ''"
+          />
+        </button>
+      </template>
+    </UCarousel>
+
     <div
       :class="
         viewport.isLessThan('tablet')
@@ -32,6 +113,50 @@
 
 <script setup>
 const viewport = useViewport();
+
+import nami from "~/assets/pages/for-brands/case-studies/nami.png";
+import homeDawn from "~/assets/pages/for-brands/case-studies/home-dawn.png";
+import namiBg from "~/assets/pages/for-brands/case-studies/nami-bg.png";
+import bonback from "~/assets/pages/for-brands/case-studies/bonback.png";
+import bonbackBg from "~/assets/pages/for-brands/case-studies/bonback-bg.png";
+import smilefam from "~/assets/pages/for-brands/case-studies/smilefam.png";
+import smilefamBg from "~/assets/pages/for-brands/case-studies/smilefam-bg.png";
+
+let items = [
+  {
+    title: "Home Dawn & Nami Korean Grill House",
+    description: `
+    <p><span class="font-semibold">Experience:</span> Korean BBQ & Casual Dining</p>
+    <p class="mb-16px"><span class="font-semibold">Deliverable Type:</span> Influencer Post</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">2</span> brands</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">10</span> nano influencers</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">21,700+</span> reached</p>`,
+    logos: [nami, homeDawn],
+    bg: namiBg,
+  },
+  {
+    title: "Bonback Bird’s Next",
+    description: `
+    <p><span class="font-semibold">Product:</span> High-Concentration Cave Bird’s Nest</p>
+    <p class="mb-16px"><span class="font-semibold">Deliverable Type:</span> Influencer Post</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">3</span> months of daily posting</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">90</span> nano influencers</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">200k+</span> reached</p>`,
+    logos: [bonback],
+    bg: bonbackBg,
+  },
+  {
+    title: "SmileFam",
+    description: `
+    <p><span class="font-semibold">Product:</span> Toothbrush and Teeth Whitening Kit</p>
+    <p class="mb-16px"><span class="font-semibold">Deliverable Type:</span> UGC content</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">1</span> month</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">2</span> products</p>
+    <p class="font-semibold"><span class="text-3xl font-bold text-primary-500">13</span> videos by unique UGC Creators</p>`,
+    logos: [smilefam],
+    bg: smilefamBg,
+  },
+];
 
 let brands = [
   "/brands/athena-scents.png",
