eab18006a86a67f7a6e8e108e4d8ad056327ca5a
AmandineH <honvaultamandine@gmail.com>
2025-02-20 21:38:06 +0800

    set default light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  colorMode: {
-    preference: "light",
-  },
   css: ["./assets/css/style.css"],
   modules: ["nuxt-viewport", "@nuxt/ui", "@nuxtjs/tailwindcss", "@nuxt/fonts"],
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: { enabled: true }
 });
