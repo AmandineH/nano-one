8bec7934d8473ca4560d9cc73ee5daaabf93626a
AmandineH <honvaultamandine@gmail.com>
2025-02-20 21:30:46 +0800

    Add color mode button

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <ClientOnly>
+      <UButton
+        :icon="
+          isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+        "
+        color="gray"
+        variant="ghost"
+        aria-label="Theme"
+        @click="isDark = !isDark"
+      />
+      <template #fallback>
+        <div class="w-8 h-8" />
+      </template>
+    </ClientOnly>
+
     <div
       class="mx-auto max-w-1300px"
       :class="
@@ -200,6 +215,16 @@
 </template>
 
 <script setup>
+const colorMode = useColorMode();
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+
 const viewport = useViewport();
 
 useHead({
